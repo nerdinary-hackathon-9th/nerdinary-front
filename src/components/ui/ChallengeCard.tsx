1cf2263c3de14186479563e97f5c81f0655ed401
2ssac <ivory.ma9ic@gmail.com>
2025-11-23 01:34:10 +0900

    chore:ChallengeList에 mockData 넣기

--- src/components/ui/ChallengeCard.tsx
+++ src/components/ui/ChallengeCard.tsx
@@ -1,25 +1,42 @@
 import CalendarIcon from '@/assets/calendar.svg?react';
 import PeopleIcon from '@/assets/people.svg?react';
 
-const ChallengeCard = () => {
+interface ChallengeCardProps {
+  title: string;
+  imgSrc: string;
+  startDate: string;
+  endDate: string;
+  participant: number;
+}
+
+const ChallengeCard = ({
+  title,
+  imgSrc,
+  startDate,
+  endDate,
+  participant = 0,
+}: ChallengeCardProps) => {
   return (
     <div className="w-5/6 h-fit flex p-5 gap-4 shadow-xs rounded-[12px] border transition-all duration-200 ease-out cursor-pointer hover:scale-105 active:scale-[1.05]">
       <div>
-        {/* <img src="" /> */}
-        <div className="w-22 h-22 object-cover rounded-md bg-neutral-200 "></div>
+        <div className="w-22 h-22 object-cover rounded-md bg-neutral-200 ">
+          <img src={imgSrc} />
+        </div>
       </div>
       <div className="py-1 flex flex-col gap-2">
-        <span className="text-sm font-medium">챌린지 제목</span>
+        <span className="text-sm font-medium">{title}</span>
         <div className="flex flex-col gap-1 text-xs text-neutral-400">
           <div className="flex items-center gap-1">
             <CalendarIcon className="w-4 h-4" />
             {/* TODO : api response body 받으면 변경  */}
-            <span>2025.11.22 ~ 2025.11.23</span>
+            <span>
+              {startDate} ~ {endDate}
+            </span>
           </div>
           <div className="flex items-center gap-1">
             <PeopleIcon className="w-4 h-4" />
             {/* TODO : api response body 받으면 변경  */}
-            <span>100 명</span>
+            <span>{participant} 명</span>
           </div>
         </div>
       </div>
--- src/page/challengeList/ChallengeList.tsx
+++ src/page/challengeList/ChallengeList.tsx
@@ -1,15 +1,52 @@
 import SearchFilterBar from '@/components/ui/SearchFilterBar';
 import ChallengeCard from '@/components/ui/ChallengeCard';
 
+const mockData = [
+  {
+    title: '갑자기 바다보고 오기!!!',
+    imgSrc: '/public/listexam1.png',
+    startDate: '2025.11.22',
+    endDate: '2025.11.23',
+    participant: 112,
+  },
+  {
+    title: '비오는 날 비 맞으면서 걷기',
+    imgSrc: '/public/listexam2.png',
+    startDate: '2025.11.23',
+    endDate: '2025.11.24',
+    participant: 55,
+  },
+  {
+    title: '본가에 강아지 보러 다녀오기',
+    imgSrc: '/public/listexam3.png',
+    startDate: '2025.11.24',
+    endDate: '2025.11.25',
+    participant: 23,
+  },
+
+  {
+    title: '겨울에 아이스크림 먹기!',
+    imgSrc: '/public/listexam4.png',
+    startDate: '2025.11.25',
+    endDate: '2025.11.26',
+    participant: 241,
+  },
+  {
+    title: '잠시 노트북 덮고 쉬자',
+    imgSrc: '/public/listexam5.png',
+    startDate: '2025.11.26',
+    endDate: '2025.11.27',
+    participant: 79,
+  },
+];
+
 const ChallengeList = () => {
   return (
     <div className="w-full flex flex-col gap-3 items-center overflow-y-scroll">
       <SearchFilterBar />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
+      {mockData.map((item, idx) => (
+        <ChallengeCard key={idx} {...item} />
+      ))}
     </div>
   );
 };
