a1ba8bfee3e3de09f7da34daa32a5e3ef7d0bd28
2ssac <ivory.ma9ic@gmail.com>
2025-11-23 01:23:05 +0900

    feat:ChallegeCard.tsx wip

--- src/components/ui/ChallengeCard.tsx
+++ src/components/ui/ChallengeCard.tsx
@@ -1,19 +1,26 @@
+import CalendarIcon from '@/assets/calendar.svg?react';
+import PeopleIcon from '@/assets/people.svg?react';
+
 const ChallengeCard = () => {
   return (
-    <div
-      className="w-5/6 h-24 flex p-5 gap-5 shadow-md rounded-md border
-                  transition-all duration-200 ease-out cursor-pointer
-                  hover:scale-105 active:scale-[1.05]"
-    >
+    <div className="w-5/6 h-fit flex p-5 gap-4 shadow-xs rounded-[12px] border transition-all duration-200 ease-out cursor-pointer hover:scale-105 active:scale-[1.05]">
       <div>
         {/* <img src="" /> */}
-        <div className="w-14 h-14 object-cover bg-neutral-200"></div>
+        <div className="w-22 h-22 object-cover rounded-md bg-neutral-200 "></div>
       </div>
-      <div className="py-0.5 flex flex-col justify-between">
-        <span className="text-sm">챌린지 제목</span>
-        <div className="flex gap-2">
-          <span className="text-xs text-neutral-400">기간</span>
-          <span className="text-xs text-neutral-400">참여자 수</span>
+      <div className="py-1 flex flex-col gap-2">
+        <span className="text-sm font-medium">챌린지 제목</span>
+        <div className="flex flex-col gap-1 text-xs text-neutral-400">
+          <div className="flex items-center gap-1">
+            <CalendarIcon className="w-4 h-4" />
+            {/* TODO : api response body 받으면 변경  */}
+            <span>2025.11.22 ~ 2025.11.23</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <PeopleIcon className="w-4 h-4" />
+            {/* TODO : api response body 받으면 변경  */}
+            <span>100 명</span>
+          </div>
         </div>
       </div>
     </div>
