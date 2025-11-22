8246128e551b3a090a67a88672e670321194a8f5
kimyounghee425 <doctorsean0425@gmail.com>
2025-11-23 08:27:59 +0900

    build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+
+import { Outlet } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
 function App() {
-  const location = useLocation();
 
-  useEffect(() => {
-    const userId = localStorage.getItem('userId');
 
-    const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
+  // useEffect(() => {
+  //   const userId = localStorage.getItem('userId');
 
-    if (!userId && !isAuthPage) {
-      window.location.replace('/login');
-    }
-  }, [location.pathname]);
+  //   const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
+
+  //   if (!userId && !isAuthPage) {
+  //     window.location.replace('/login');
+  //   }
+  // }, [location.pathname]);
 
   return (
     <>
