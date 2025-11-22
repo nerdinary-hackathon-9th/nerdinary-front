79caa17e42868e83b5652a96a44470fe52e47d6b
2ssac <ivory.ma9ic@gmail.com>
2025-11-22 21:35:09 +0900

    style:css 수정

--- src/components/ui/SearchFilterBar.tsx
+++ src/components/ui/SearchFilterBar.tsx
@@ -1,9 +1,13 @@
 const SearchFilterBar = () => {
   return (
-    <div className="w-5/6 flex gap-4 py-3">
-      <input type="text" placeholder="검색창" className="flex-1 bg-muted py-2 px-3 rounded-md" />
+    <div className="w-5/6 flex gap-3 py-3">
+      <input
+        type="text"
+        placeholder="검색창"
+        className="flex-1 bg-muted py-2 px-3 rounded-md border"
+      />
       <div className="relative">
-        <select className="bg-muted py-3 px-3 pr-8 rounded-md text-sm cursor-pointer outline-none appearance-none w-28">
+        <select className="bg-muted py-3 px-3 pr-8 rounded-md text-sm cursor-pointer outline-none appearance-none w-20 border">
           <option value="" disabled>
             필터
           </option>
